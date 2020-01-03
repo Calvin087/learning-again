2f4aa4ed5c7036983b408be7c331b03d0340afba
Calvin087 <58273749+Calvin087@users.noreply.github.com>
2020-01-03 17:26:21 +0100

    update

---
+++ js-bootcamp/advanced/conditional-operator.js
@@ -0,0 +1,15 @@
+// const myAge = 27
+// const message = myAge >= 18 ? 'You can vote!' : 'You cannot vote'
+// console.log(message)
+
+const myAge = 32
+
+const showPage = () => {
+    console.log('Showing the page')
+}
+
+const showErrorPage = () => {
+    console.log('Showing the error page')
+}
+
+myAge >= 21 ? showPage()
