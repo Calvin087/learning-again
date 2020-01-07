7f585b0c2dd3da60cfbc1607d7b07e3ff2223e22
Calvin087 <58273749+Calvin087@users.noreply.github.com>
2020-01-07 18:39:06 +0100

    pop

--- js-bootcamp/functions/grade-calc.js
+++ js-bootcamp/functions/grade-calc.js
@@ -1,25 +1,35 @@
 // Correct Version
 
 let gradeCalc = function (score, totalScore) {
-    let percent = (score / totalScore) * 100
-    let letterGrade = ``
+    
+    if(typeof score === "number" && typeof totalScore === "number") {
 
-    if (percent >=90) {
-        letterGrade = `A`
-    } else if (percent >=80) {
-        letterGrade = `B`
-    } else if (percent >=70) {
-        letterGrade = `C`
-    } else if (percent>= 60) {
-        letterGrade = `D`
-    } else {
-        letterGrade = `F`
+        let percent = (score / totalScore) * 100
+        let letterGrade = ``
+    
+        if (percent >=90) {
+            letterGrade = `A`
+        } else if (percent >=80) {
+            letterGrade = `B`
+        } else if (percent >=70) {
+            letterGrade = `C`
+        } else if (percent>= 60) {
+            letterGrade = `D`
+        } else {
+            letterGrade = `F`
+        }
+    
+        return `You got a ${letterGrade} (${percent}%)`
+
     }
-
-    return `You got a ${letterGrade} (${percent}%)`
-
 }
 
-let result = gradeCalc(10,100)
-console.log(result)
+try {
+    let result = gradeCalc(10, 100)
+    console.log(result)
+} catch (e) {
+    console.log(`you dummy, you didn't enter a number`)
+}
+
+
 
