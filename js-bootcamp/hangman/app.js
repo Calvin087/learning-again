1d9555974c3f144ffac055d029a6144b4789c22b
Calvin087 <58273749+Calvin087@users.noreply.github.com>
2020-01-15 18:38:42 +0100

    5am study

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -20,11 +20,27 @@ window.addEventListener('keypress', function (e) {
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', () => {
-    if(e.target.readyState === 4) {
+    if(e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place')
     }
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=4')
 request.send()
+
+const countryCode = 'ES'
+const countryRequest = new XMLHttpRequest()
+
+countryRequest.addEventListener('readystatechange', (e) => {
+    if(e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((country) => country.alpha2Code === countryCode)
+        console.log(country.name)
+    }
+})
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+countryRequest.send()
