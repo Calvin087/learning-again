852fab7df120b4873068952b9b4ef0d8b3edd3af
Calvin087 <58273749+Calvin087@users.noreply.github.com>
2020-02-13 05:55:59 +0100

    more

--- react-course-projects/expensify-app/src/playground/redux-101.js
+++ react-course-projects/expensify-app/src/playground/redux-101.js
@@ -19,7 +19,7 @@ const resetCount = () => ({
     type: 'RESET'
 })
 
-const store = createStore((state = { count: 0}, action) => {
+const countReducer = (state = { count: 0}, action) => {
     switch (action.type) {
         case 'INCREAMENT':
         return {
@@ -40,7 +40,9 @@ const store = createStore((state = { count: 0}, action) => {
         default:
             return state
     }
-})
+}
+
+const store = createStore(countReducer)
 
 const unsub = store.subscribe(() => {
     console.log(store.getState())
