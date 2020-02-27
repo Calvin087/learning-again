9c7ded5dd4a3e6db3553a5c11bc7379dee5c9d2f
Calvin087 <58273749+Calvin087@users.noreply.github.com>
2020-02-27 20:18:55 +0100

    jest training

---
+++ react-course-projects/expensify-app/src/tests/selectors/expenses.test.js
@@ -0,0 +1,42 @@
+import selectExpenses from '../../selectors/expenses'
+import moment from 'moment'
+
+const expenses = [{
+    id: '1',
+    description: 'Gum',
+    note: '',
+    amount: 195,
+    createdAt: 0
+}, {
+    id: '2',
+    description: 'Rent',
+    note: '',
+    amount: 109500,
+    createdAt: moment(0).subtract(4, 'days'), valueOf()
+}, {
+    id: '3',
+    description: 'Credit card',
+    note: '',
+    amount: 4500,
+    createdAt: 1000
+}]
+
+test('should filter by text value', () => {
+    const filters = {
+        text: 'e',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    }
+    const result = selectExpenses(expenses, filters)
+    expect(result).toEqual([ expenses[2], expenses[1] ])
+})
+
+test('filter by start date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: moment(0),
+        endDate: undefined
+    }
+})
