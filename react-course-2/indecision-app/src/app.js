a1ba9238be7b3c21e835aab9ee076e4426dc57fd
Calvin-T <calvin+1@sliame.com>
2020-05-18 07:07:45 +0200

    morning training

--- react-course-2/indecision-app/src/app.js
+++ react-course-2/indecision-app/src/app.js
@@ -1,1 +1,7 @@
-console.log('daviuhie')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import validator from 'validator'
+
+const ymum = <p>test</p>
+
+ReactDOM.render(ymum, document.getElementById('app'))
