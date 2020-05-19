3ed6afebd804c895d6a650f05abe200b87437a0e
Calvin-T <calvin+1@sliame.com>
2020-05-19 07:07:19 +0200

    red eye learning

--- react-course-2/indecision-app/webpack.config.js
+++ react-course-2/indecision-app/webpack.config.js
@@ -6,14 +6,21 @@ module.exports = {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
-    module: { //babel rules for app
+    module: {
         rules: [{
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
-    }]
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
     },
-    devtool: 'cheap-module-eval-source-map', // packge that shows me the exact path to errors, not babel files, but mine.
+    devtool: 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public')
     }
