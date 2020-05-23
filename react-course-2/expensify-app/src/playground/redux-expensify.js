162756684cd2d89643a65f1cd537bb0767f77226
Calvin-T <calvin+1@sliame.com>
2020-05-23 16:32:16 +0200

    homework

--- react-course-2/expensify-app/src/playground/redux-expensify.js
+++ react-course-2/expensify-app/src/playground/redux-expensify.js
@@ -19,6 +19,40 @@ const addExpense = ( // This is being added each time, i confuse expenses with e
     }
 })
 
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+})
+
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+})
+
+const setTextFilter = (text) => ({
+    type: 'SET_TEXT_FILTER',
+    text
+})
+
+const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+})
+
+const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+})
+
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+})
+
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+})
+
 const expenseReducerDefaultState = []
 
 const expenseReducer = (state = expenseReducerDefaultState, action) => {
@@ -30,9 +64,22 @@ const expenseReducer = (state = expenseReducerDefaultState, action) => {
             ]
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => {
+// state.filter(expense) OR { id } expense is an obeject inside an array,
+// so we can destructure the expense object, pull the ID of each object by
+// naming it this way.
                 return id !== action.id
             })
-
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if(expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                } else {
+                    return expense
+                }
+            })
         default:
             return state
     }
@@ -47,9 +94,51 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            }
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            }
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            }
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            }
         default:
             return state
     }
+}
+
+// TimeStamps
+
+// Visible Expenses
+
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+// inside sort.subscribe, i have a const which points to getVisibleExpenses
+// inside visible i'm calling the function here and passing both reducers.
+// I'm now receiving two reducers and destructuring them as arguments.
+    return expenses.filter(() => {
+        const startDateMatch
+        const endDateMatch
+        const textMatch
+
+        return startDateMatch && endDateMatch && textMatch
+    })
 }
 
 // Store Creation
@@ -62,12 +151,22 @@ const store = createStore(
 )
 
 store.subscribe(() => {
-    console.log(store.getState())
+    const state = store.getState()
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+    console.log(visibleExpenses)
 })
 
 const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, note: "Call to edit" }))
 
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id}))
 
+// store.dispatch(removeExpense({ id: expenseOne.expense.id}))
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 3000 }))
+// store.dispatch(setTextFilter('Car'))
+
+// store.dispatch(sortByAmount())
+// // store.dispatch(sortByDate())
+// store.dispatch(setStartDate(125))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(1250))
