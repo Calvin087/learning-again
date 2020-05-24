f03accb47527426c038735270e0915fb69de2e46
Calvin-T <calvin+1@sliame.com>
2020-05-24 17:53:27 +0200

    updates

--- react-course-2/expensify-app/src/app.js
+++ react-course-2/expensify-app/src/app.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import AppRouter from './routers/AppRouter'
+import configureStore from './store/configureStore'
+import {addExpense} from './actions/expenses'
+import {setTextFilter} from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-import AppRouter from './routers/AppRouter'
+
+const store = configureStore()
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: -1000 }))
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 1200, createdAt: 2000 }))
+
+store.dispatch(setTextFilter('water'))
+
+const state = store.getState()
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
