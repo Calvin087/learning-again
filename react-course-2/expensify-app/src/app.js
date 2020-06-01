0d40eaf2e46f69d5243d1781c81cedffcd909330
Calvin-T <calvin+1@sliame.com>
2020-06-01 11:46:47 +0200

    updates

--- react-course-2/expensify-app/src/app.js
+++ react-course-2/expensify-app/src/app.js
@@ -12,14 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 1590746400000 }))
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 1000, createdAt: 2000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: -12000 }))
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
-
 const jsx = ( // This gives the whole app access to the store as a prop
     <Provider store={store}>
         <AppRouter />
