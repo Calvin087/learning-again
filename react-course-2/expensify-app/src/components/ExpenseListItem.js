afae78d99f1159dcfef37c58132e629c49b568f8
Calvin-T <calvin+1@sliame.com>
2020-06-02 06:47:53 +0200

    currency update

--- react-course-2/expensify-app/src/components/ExpenseListItem.js
+++ react-course-2/expensify-app/src/components/ExpenseListItem.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
+import 'numeral/locales/es-es'
+
+numeral.locale('es-es');
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
     <Link to={`/edit/${id}`}>
         <h1>{description}</h1>
     </Link>
-    <p>{amount} {createdAt}</p>
+    <p>
+        {numeral(amount / 100).format('$0,0.00')}
+        -
+        {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
     </div>
 )
 
