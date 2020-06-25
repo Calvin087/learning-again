a270b2b9ad0146ffdcd46a9858b21de91d977b76
Calvin-T <calvin+1@sliame.com>
2020-06-25 05:33:44 +0200

    connection to monogdb fixed

--- mern-app/app.js
+++ mern-app/app.js
@@ -1,6 +1,9 @@
 const express = require("express")
-const app = express()
-// This creates a new Express server.
+const app = express() // This creates a new Express server.
+
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
 
 const db = require('./config/keys').mongoURI;
 const mongoose = require("mongoose");
@@ -14,9 +17,16 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello Dave"));
 //basic route so that we can render some information on our page
+app.use("/api/users", users);
+app.use("/api/tweets", tweets)
+//basic route so that we can render some information on our page
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Parse the JSON From the routes
+
 
 const port = process.env.PORT || 5000;
-// we need to tell our app which port to run on.
+// we need to tell our app which port to run/listen on.
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 // tell Express to start a socket and listen for connections on the path. 
---
+++ mern-app/routes/api/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+// http://localhost:5000/api/tweets/test
+
+module.exports = router;
