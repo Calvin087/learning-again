621a46633f99a5ae9eba9aa86ffe31f8a67c58fc
Calvin-T <calvin+1@sliame.com>
2020-06-27 18:03:01 +0200

    api fixed, content for blog is correctly loading from wordpress

--- reactNwordpress/src/components/Blog.js
+++ reactNwordpress/src/components/Blog.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import base from '../styles/base.css'
 
 function Blog() {
     const [ posts, setPosts ] = useState([])
@@ -21,16 +22,19 @@ function Blog() {
 
     return (
         <div>
-            {posts.map((post, index) => (
-                <Link to={`/post/${post.slug}`} key={post.id}>
-                    <h2>{post.title.rendered}</h2>
-                    
-                    <img src={post._embedded['wp:featuredmedia'][0].source_url} /> {/* Apparently this is bracket notation */}
-                    <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></p>
-                </Link>
-            ))}
+            <div className="blog-post-parent">
+                {posts.map((post, index) => (
+                    <Link to={`/post/${post.slug}`} key={post.id}>
+                        <h2>{post.title.rendered}</h2>
+                        <img style={{ width: "200px" }} src={post.acf.featured_image_png} />
+                        <img style={{ width: "200px" }} src={post._embedded['wp:featuredmedia'][0].source_url} /> {/* Apparently this is bracket notation */}
+                        <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></p>
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
 
 export default Blog
+    
--- reactNwordpress/src/components/BlogPost.js
+++ reactNwordpress/src/components/BlogPost.js
@@ -1,40 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, Fragment } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import base from '../styles/base.css'
 
-function BlogPost() {
-    const [ posts, setPosts ] = useState([])
-    let { slug } = useParams()
-
-    // console.log(slug);
+function Blog() {
+    const [posts, setPosts] = useState([])
+    const { slug } = useParams()
+    const post = posts.filter(post => post.slug === slug)
     
-    function findPostBySlug(slug) {
-        return posts.find(i => i.slug === slug)
-    }
-
-    let dave = findPostBySlug(slug)
-    console.log(dave);
-    
+    const siteURL = "http://calvint1.sg-host.com/wp-json/wp/v2/books?_embed"
 
     useEffect(() => {
         async function loadposts() {
-            const response = await fetch(`http://calvint1.sg-host.com/wp-json/wp/v2/books`)
-            if(!response.ok) {
+            const response = await fetch(siteURL)
+            if (!response.ok) {
                 console.log("not wokring");
                 return
             }
 
             const posts = await response.json()
             setPosts(posts)
-
         }
         loadposts()
     }, [])
 
     return (
-      <div>
+        <div>
+            <div className="blog-post">
+                {post.map((post, index) => (
+                    <Fragment key={post.id} >
+                    
+                    <div className="header-background">
+                    
+                        <h1 className="blog-post-h1">{post.title.rendered}</h1>
+                    
+                        <div className="header">            
+                                    <p className="header-left" dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}></p>
+                                    <img className="header-right" src={post.acf.featured_image_png} />
+                        </div>
+                        
+                    </div>
 
-      </div>
-    );
+                        <p className="blog-content" dangerouslySetInnerHTML={{ __html: post.content.rendered }}></p>
+
+                    </Fragment>
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default BlogPost
+export default Blog
+    // < img className = "blog-feature-img" src = { post._embedded['wp:featuredmedia'][0].source_url } /> {/* Apparently this is bracket notation */ }
